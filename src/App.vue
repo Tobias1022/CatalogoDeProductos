9327236b8f3c0a4a60b0ed4ee4ecaca5d209a262
tobias1022 <tobiasvalenzuela63@gmail.com>
2024-09-15 19:39:23 -0300

    terminado

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,20 @@
 <template>
-  <div class="col-md-4 mb-3">
-    <div class="card">
-      <img :src="producto.imagen" class="card-img-top" :alt="producto.nombre">
-      <div class="card-body">
-        <h5 class="card-title">{{ producto.nombre }}</h5>
-        <p class="card-text">{{ producto.descripcion }}</p>
-        <p class="card-text"><strong>Precio: ${{ producto.precio }}</strong></p>
-      </div>
-    </div>
-
-</div>
+  <div id="app">
+    <Producto2 />
+  </div>
 </template>
 
+<script>
+import Producto2 from './components/Producto2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Producto2
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Producto2.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container mt-5 bg-light py-4 rounded">
+    <div class="row">
+      <div class="col-md-4 mb-4" v-for="producto in productos" :key="producto.id">
+        <div class="card">
+          <img :src="producto.thumbnail" class="card-img-top" :alt="producto.name">
+          <div class="card-body">
+            <h5 class="card-title">{{ producto.name }}</h5>
+            <p class="card-text">{{ producto.description }}</p>
+            <p class="card-text"><strong>${{ producto.price }}</strong></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name:'Producto2',
+  data() {
+    return {
+      productos: []  
+    };
+  },
+  mounted() {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => {
+        this.productos = data.products; 
+      })
+      .catch(error => console.error('Error fetching products:', error));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
